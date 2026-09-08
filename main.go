a9797589ce45b6051230415fceb10e1a662578af
nicos92 <nicolassandoval140692@gmail.com>
2026-09-08 06:59:31 -0300

    Establecer dimensiones mínimas de ventana y actualizar enlace

--- main.go
+++ main.go
@@ -17,15 +17,17 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "imp-ticket",
-		Width:  1024,
-		Height: 768,
+		Title:     "imp-ticket",
+		Width:     1024,
+		Height:    768,
+		MinWidth:  600,
+		MinHeight: 400,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
-		Bind: []interface{}{
+		Bind: []any{
 			app,
 		},
 	})
